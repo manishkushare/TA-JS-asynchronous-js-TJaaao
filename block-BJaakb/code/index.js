9bf5fe0ea50719211e34f2e2bde8e2a467a96c98
Manish Kushare <=>
2021-05-29 15:42:50 +0530

    implemented using promise

--- block-BJaakb/code/index.js
+++ block-BJaakb/code/index.js
@@ -3,14 +3,16 @@ const url = `https://api.unsplash.com/photos?client_id=ruzQetxrNoKE_gcFpQpUhCsBN
 const getSearchUrl = (query)=> `https://api.unsplash.com/search/photos?client_id=ruzQetxrNoKE_gcFpQpUhCsBNFuBVQ4ceius0ak7Tsc&query= ${query}`;
 let imageContainer = document.querySelector(".gallery");
 
-function fetch(url,successHandler){
+function fetch(url){
+  return new Promise((resolve,reject)=>{
     let xhr = new XMLHttpRequest();
-    xhr.open("GET",url);
-    xhr.onload = () => successHandler(JSON.parse(xhr.response));
-    xhr.onerror = ()=> console.log("something went wrong");
+    xhr.open("GET", url);
+    xhr.onload = ()=> resolve(JSON.parse(xhr.response)); 
+    xhr.onerror = () =>reject("Something went wrong");
     xhr.send();
+  })
 }
- 
+
 function displayImage(image){
   imageContainer.innerHTML = "";
   let ul = document.createElement("ul");
@@ -24,14 +26,12 @@ function displayImage(image){
   imageContainer.append(ul);
 }
 
-fetch(url,displayImage);
+fetch(url).then(displayImage);
 
 function handleInput(event){
   if(event.keyCode === 13){
     console.log("test");
-    fetch(getSearchUrl(input.value),(searchResult)=> {
-      displayImage(searchResult.results)
-    });
+    fetch(getSearchUrl(input.value)).then((searchInfo)=> displayImage(searchInfo.results));
     input.value = "";
   }
 }
